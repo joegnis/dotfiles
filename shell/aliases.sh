90a2be106df473fca0a8cae24549bc27829e27e6
Yifan Liao <yifanyangliao@gmail.com>
2017-03-22 00:50:48 -0700

    Fix `which` doesn't work when advanced options are not available

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,5 +1,11 @@
+# Better which
 which () {
-    (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-tilde --show-dot $@
+    /usr/bin/which --read-alias --read-functions --show-title --show-dot --tty-only &>/dev/null
+    if [ "$?" -eq 2 ]; then
+        /usr/bin/which $@
+        return
+    fi
+    (alias; declare -f) | /usr/bin/which --tty-only --read-alias --read-functions --show-title --show-dot $@
 }
 
 # custome sudo
