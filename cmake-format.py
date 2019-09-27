07e100de8ec52176c36f0732298fcfa6f9359db8
joegnis <joegnis@users.noreply.github.com>
2019-09-27 14:54:10 -0400

    Adds cmake-format config file

---
+++ cmake-format.py
@@ -0,0 +1,9 @@
+# If a statement is wrapped to more than one line, than dangle the closing
+# parenthesis on it's own line
+dangle_parens = True
+
+# How many spaces to tab for indent
+tab_size = 2
+
+# If arglists are longer than this, break them always
+max_subargs_per_line = 3
