fdfa45409eab92c2b3e90799c2c4ea1dbba0b465
joegnis <joegnis@users.noreply.github.com>
2019-10-02 11:47:02 -0400

    [shell] Removes ls aliases, and aliases ls to exa

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,3 +4,8 @@ alias mysudo='sudo -E env "PATH=$PATH"'
 # pretty print json
 alias prettyjson='python -m json.tool'
 
+# exa as ls
+if command_exists exa; then
+    alias ls=exa
+    alias la='exa --long --all --classify'
+fi
