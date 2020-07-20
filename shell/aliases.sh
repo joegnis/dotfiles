7242972c0584f70598ff29ddf7f18b0f59054688
Kai Liao <joegnis@pm.me>
2020-07-19 23:32:51 -0400

    Adds prettyjson alias command

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,7 +2,10 @@
 alias mysudo='sudo -E env "PATH=$PATH"'
 
 # pretty print json
-alias prettyjson='python -m json.tool'
+alias prettyjson='python -c"# -*- coding: UTF-8 -*-
+import fileinput, json;
+print(json.dumps(json.loads(\"\".join(fileinput.input())),
+    sort_keys=True, indent=4, ensure_ascii=False).encode(\"utf-8\"))"'
 
 # exa as ls
 if command_exists exa; then
